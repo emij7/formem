198a6b1de6a483b1a2507c0da4113963d265280b
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-01-09 14:33:19 -0300

    feat: index with router

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import ErrorPage from "./error-page";
+import { Header } from "./components/molecule/header/header";
+import { Footer } from "./components/molecule/footer/footer";
+import { Results } from "./routes/results";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles, theme } from "./styles/global";
+import { Main } from "./components/molecule/main/main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "results",
+    element: <Results />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Header />
+      <RouterProvider router={router} />
+      <Footer />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
