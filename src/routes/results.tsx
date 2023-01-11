6ab2dd60e53a24c4451a5df953ae390b9054b978
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-01-11 11:24:03 -0300

    feat: no result redirects to home

--- src/routes/results.tsx
+++ src/routes/results.tsx
@@ -1,6 +1,19 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
 import { Content } from "../components/atoms/content/content";
 
 export const Results: FC = () => {
-  return <Content>RESULTADOS</Content>;
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  useEffect(() => {
+    const checkData = (state: object) => {
+      if (state === null) {
+        navigate("/");
+      }
+    };
+
+    checkData(state);
+  }, [navigate, state]);
+
+  return <Content>RESULTADOS </Content>;
 };
