579f5f31552e60338d54d675936793ec7cf11ca8
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-01-11 13:39:09 -0300

    feat: message and result

--- src/components/molecule/message/message.tsx
+++ src/components/molecule/message/message.tsx
@@ -3,6 +3,7 @@ import { Content } from "../../atoms/content/content";
 import { TMessage } from "./message.types";
 import { useNavigate } from "react-router-dom";
 import { Text } from "../../atoms/text/text";
+import { theme } from "../../../styles/global";
 
 export const Message: FC<TMessage> = ({ data }) => {
   const navigate = useNavigate();
@@ -14,11 +15,28 @@ export const Message: FC<TMessage> = ({ data }) => {
     });
   };
   return (
-    <Content as="section" align="center">
-      <Text>{`${data.full_name} tu formulario fue enviado correctamente!`}</Text>
-      <Text>Puedes ver tus resultados haz click aquí: </Text>
-      <Content onClick={onViewResults}>
-        <Text>RESULTADOS</Text>
+    <Content as="section" align="center" margin={`${theme.spacing.m} 0`}>
+      <Text
+        align="center"
+        size={theme.size.m}
+      >{`¡${data.full_name} tu formulario fue enviado correctamente!`}</Text>
+      <Text align="center" size={theme.size.m}>
+        Para ver tus resultados haz click aquí:{" "}
+      </Text>
+      <Content
+        align="center"
+        backgroundColor={theme.color.blue}
+        border={`1px solid ${theme.color.gray.normal}`}
+        borderRadius={theme.size.s}
+        margin={`${theme.spacing.m} 0`}
+        onClick={onViewResults}
+        padding={theme.spacing.m}
+        max-width="50%"
+        width="15rem"
+      >
+        <Text align="center" size={theme.size.m}>
+          RESULTADOS
+        </Text>
       </Content>
     </Content>
   );
--- src/routes/results.tsx
+++ src/routes/results.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router";
 import { Content } from "../components/atoms/content/content";
+import { Text } from "../components/atoms/text/text";
+import { theme } from "../styles/global";
 
 export const Results: FC = () => {
   const navigate = useNavigate();
@@ -15,5 +17,51 @@ export const Results: FC = () => {
     checkData(state);
   }, [navigate, state]);
 
-  return <Content>RESULTADOS </Content>;
+  const capitalizeFirstLetter = (str: string) => {
+    const capitalized = str.charAt(0).toUpperCase() + str.slice(1);
+    return capitalized;
+  };
+
+  return (
+    <Content
+      align="center"
+      as="section"
+      backgroundColor={theme.color.primary.light}
+      borderRadius={theme.size.s}
+      margin="auto"
+      minHeight="5rem"
+      width="80%"
+      padding={`${theme.spacing.l} 0`}
+    >
+      <Text size={theme.size.m}>Estos son los datos que compartiste:</Text>
+      <Content align="center" margin={`${theme.spacing.l} 0`}>
+        <Content align="center" margin={`${theme.spacing.s} 0`} gap="0">
+          <Text size={theme.size.m} semibold>
+            Nombre:
+          </Text>
+          <Text size={theme.size.m}>{state.data.full_name}</Text>
+        </Content>
+        <Content align="center" margin={`${theme.spacing.s} 0`} gap="0">
+          <Text size={theme.size.m} semibold>
+            E-Mail:
+          </Text>
+          <Text size={theme.size.m}>{state.data.email}</Text>
+        </Content>
+        <Content align="center" margin={`${theme.spacing.s} 0`} gap="0">
+          <Text size={theme.size.m} semibold>
+            Nacionalidad:
+          </Text>
+          <Text size={theme.size.m}>
+            {capitalizeFirstLetter(state.data.country_of_origin)}
+          </Text>
+        </Content>
+        <Content align="center" margin={`${theme.spacing.s} 0`} gap="0">
+          <Text size={theme.size.m} semibold>
+            Fecha de nacimiento:
+          </Text>
+          <Text size={theme.size.m}>{state.data.birth_date}</Text>
+        </Content>
+      </Content>
+    </Content>
+  );
 };
